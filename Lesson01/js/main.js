2ae4fa0aa309661fd669ad6f3b6dd170c6414ac8
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2021-01-07 20:54:35 -0300

    Lesson01

---
+++ Lesson01/js/main.js
@@ -0,0 +1,10 @@
+var nome = "kaik";
+var idade = 20
+var frase = "Japao é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+console.log(nome);
+console.log(idade);
+
+// .replace = alterar string
+console.log(frase);
+console.log(frase.replace("Japao", "Brasil"));
