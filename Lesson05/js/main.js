8df2d1f641545c6d1662a0d960f7e72a20481dbd
KaikOliveira <69028381+KaikOliveira@users.noreply.github.com>
2021-02-11 20:18:01 -0300

    up

--- Lesson05/js/main.js
+++ Lesson05/js/main.js
@@ -25,3 +25,23 @@ alert(d);
 console.log(d.getDay());
 console.log(d.getHours());
 console.log(d.getMinutes());
+
+// Repetição 
+var count = 0;
+while (count <= 5) {
+  console.log(count);
+  count ++;
+}
+
+// Repetição -> for
+var cont1;
+for(cont1=1; cont1 <= 5; cont1++) {
+  console.log(cont1)
+}
+
+// Data no JavaScript
+var d = new Date();
+alert(d);
+console.log(d.getDay());
+console.log(d.getHours());
+console.log(d.getMinutes());
